0031fe7e7b36e0991b30070d64af71b27b10543f
Maunder <scott.maunder@ezyvet.com>
2019-10-25 13:09:51 +1300

    Added margin to the component when inside of the titlebar

--- src/components/NavBar/CustomBranchSelector/CustomBranchSelector.tsx
+++ src/components/NavBar/CustomBranchSelector/CustomBranchSelector.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import BranchNameTag from './BranchNameTag';
+import './style.css';
+
+const BranchTagRenderer = (branches: string[]) => {
+  return branches.map(branch => {
+    return <BranchNameTag branchName={branch} />;
+  });
+};
 
 export const CustomBranchSelector = (props: {
   selectedBranches: string[];
   availableBranches: string[];
 }) => {
-  return (
-    <div>
-      <BranchNameTag branchName={'One1'} />
-      <BranchNameTag branchName={'Two2'} />
-    </div>
-  );
+  const branches = BranchTagRenderer(props.selectedBranches);
+  return <div className="branchSelector">{branches}</div>;
 };
 
 export default CustomBranchSelector;
