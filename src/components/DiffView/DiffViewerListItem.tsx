ab0c3f8c8f3ead9cade7a2bd92060116464b260e
Maunder <maunderscott@gmail.com>
2019-11-03 15:21:40 +1300

    Adding underline and cleaning up the styling

--- src/components/DiffView/DiffViewerListItem.tsx
+++ src/components/DiffView/DiffViewerListItem.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Collapse, Icon } from '@blueprintjs/core';
+import { Collapse, Icon, Elevation, Card } from '@blueprintjs/core';
 import Diff from './Diff';
 import { IconNames } from '@blueprintjs/icons';
 import './header.css';
@@ -33,25 +33,31 @@ export default class DiffViewerListItem extends React.Component<
 
     return (
       <div>
-        <div
-          onClick={() => onClickCollapse(this.props.index)}
-          className={'headerContainer'}
-        >
-          <span className={'headerTitle'}>
-            <Icon icon={IconNames.CODE} iconSize={25} />
-            {'  '}
-            <b>{fileName}</b>
-          </span>
-          <span className={'headerCollapseIcon'}>
-            <Icon
-              icon={isOpen ? IconNames.CHEVRON_UP : IconNames.CHEVRON_DOWN}
-              iconSize={25}
-            />
-          </span>
-        </div>
-        <Collapse isOpen={isOpen} keepChildrenMounted={true}>
-          <Diff hunks={hunks} diffType={diffType} oldSource={oldSource} />
-        </Collapse>
+        <Card elevation={Elevation.ONE}>
+          <div
+            onClick={() => onClickCollapse(this.props.index)}
+            className={'headerContainer'}
+          >
+            <span className={'headerTitle'}>
+              <Icon
+                icon={IconNames.CODE}
+                iconSize={25}
+                style={{ paddingRight: '10px' }}
+              />
+              <b>{fileName}</b>
+            </span>
+            <span className={'headerCollapseIcon'}>
+              <Icon
+                icon={isOpen ? IconNames.CHEVRON_UP : IconNames.CHEVRON_DOWN}
+                iconSize={25}
+              />
+            </span>
+          </div>
+          <hr />
+          <Collapse isOpen={isOpen} keepChildrenMounted={true}>
+            <Diff hunks={hunks} diffType={diffType} oldSource={oldSource} />
+          </Collapse>
+        </Card>
       </div>
     );
   }
