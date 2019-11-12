b4b6519410f5e9f28e29ec4129d0ae684f7240f3
Maunder <scott.maunder@ezvet.com>
2019-11-12 13:33:48 +1300

    Adding template definition file. Upgrading and cleaning dependencies

---
+++ src/types/react-lines-ellipsis.d.ts
@@ -0,0 +1,8 @@
+declare module "react-lines-ellipsis" {
+  import React = __React;
+
+  // Example
+  function test(): (test: React.Component<any, any>) => React.Component<any, any>;
+
+  export = withSideEffect;
+}
