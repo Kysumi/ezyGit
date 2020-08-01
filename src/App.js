ca9619f429d6e12b15f8a8ad063762c9f5029a2c
Kysumi <maunderscott@gmail.com>
2020-08-01 13:09:35 +1200

    Now has responsibe sidebar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
-import React from 'react';
-import { Box, Button, Heading, Grommet } from 'grommet';
+import React, { useState } from 'react';
 import { Notification } from 'grommet-icons';
+import {
+  Box,
+  Button,
+  Collapsible,
+  Heading,
+  Grommet,
+  ResponsiveContext,
+} from 'grommet';
 
 const theme = {
   global: {
@@ -30,30 +37,43 @@ const AppBar = (props) => (
 );
 
 function App() {
+  const [showSidebar, setShowSidebar] = useState(false);
   return (
     <Grommet theme={theme} full>
-      <Box fill>
-        <AppBar>
-          <Heading level="3" margin="none">
-            ezyGit
-          </Heading>
-          <Button icon={<Notification />} onClick={() => {}} />
-        </AppBar>
-        <Box direction="row" flex overflow={{ horizontal: 'hidden' }}>
-          <Box flex align="center" justify="center">
-            app body
+      <ResponsiveContext.Consumer>
+        {(size) => (
+          <Box fill>
+            <AppBar>
+              <Heading level="3" margin="none">
+                ezyGit
+              </Heading>
+              <Button
+                icon={<Notification />}
+                onClick={() => setShowSidebar(!showSidebar)}
+              />
+            </AppBar>
+            <Box direction="row" flex overflow={{ horizontal: 'hidden' }}>
+              <Box flex align="center" justify="center">
+                app body
+              </Box>
+              {size !== 'small' && (
+                <Collapsible direction="horizontal" open={showSidebar}>
+                  <Box
+                    flex
+                    width="medium"
+                    background="light-2"
+                    elevation="small"
+                    align="center"
+                    justify="center"
+                  >
+                    sidebar
+                  </Box>
+                </Collapsible>
+              )}
+            </Box>
           </Box>
-          <Box
-            width="medium"
-            background="light-2"
-            elevation="small"
-            align="center"
-            justify="center"
-          >
-            sidebar
-          </Box>
-        </Box>
-      </Box>
+        )}
+      </ResponsiveContext.Consumer>
     </Grommet>
   );
 }
