5bdeb7451164b0bc88b7aa1b97fd09974293bf5b
Kysumi <maunderscott@gmail.com>
2020-08-06 14:49:58 +1200

    Adding current checked out branch name to app bar

--- src/components/appbar/AppBar.js
+++ src/components/appbar/AppBar.js
@@ -8,14 +8,22 @@ import {
   NavbarDivider,
   Classes,
 } from '@blueprintjs/core';
+import { useSelector } from 'react-redux';
+import { getBranchNameSelector } from '../../store/repo/RepoSelector';
 
 export const AppBar = () => {
+  const branchName = useSelector(getBranchNameSelector);
+
   return (
     <Navbar>
       <NavbarGroup>
-        <NavbarHeading>ezyGit</NavbarHeading>
+        <NavbarHeading>
+          <h2>ezyGit</h2>
+        </NavbarHeading>
         <NavbarDivider />
-        Branch:
+        <div className={Classes.TEXT_LARGE}>
+          Branch: <b>{branchName}</b>
+        </div>
       </NavbarGroup>
       <NavbarGroup align={Alignment.RIGHT}>
         <NavbarDivider />
