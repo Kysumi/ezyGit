90d78b99da68b9027380553f058ffbb51d37c31c
Maunder <maunderscott@gmail.com>
2020-08-09 15:04:40 +1200

    Adding doc blocks and the difference helper function

--- src/git/git.js
+++ src/git/git.js
@@ -1,6 +1,14 @@
+import { diffLines, formatLines } from 'unidiff';
+import { parseDiff } from 'react-diff-view';
 const git = require('isomorphic-git');
 var fs = window.require('fs');
 
+/**
+ * Returns the previous commits from the provided branch
+ *
+ * @param {string} filePath
+ * @param {string} branch
+ */
 export const getCommitLog = async (filePath, branch) => {
   return await git.log({
     fs,
@@ -10,10 +18,30 @@ export const getCommitLog = async (filePath, branch) => {
   });
 };
 
+/**
+ * Returns the string name of the current checked out branch
+ *
+ * @param {string} filePath
+ * @returns {Promise<string|void>}
+ */
 export const getCurrentBranch = async (filePath) => {
   return await git.currentBranch({
     fs,
     dir: filePath,
     fullname: false,
   });
+};
+
+/**
+ * Gets the difference between the two strings in a format for the
+ * react-diff-view lib
+ *
+ * @param {string} originalText
+ * @param {string} changedText
+ */
+export const getGitDifference = (originalText, changedText) => {
+  const diffText = formatLines(diffLines(originalText, changedText), {
+    context: 3,
+  });
+  return parseDiff(diffText, { nearbySequences: 'zip' });
 };
