38d2b3e0f4fc7e3192b6ef45060952aeacccb8d2
Maunder <maunderscott@gmail.com>
2020-08-10 17:23:40 +1200

    Adding functionality to allow the user to collapse diffd

--- src/components/difflist/DiffList.js
+++ src/components/difflist/DiffList.js
@@ -1,52 +1,26 @@
 import React from 'react';
 import ReactList from 'react-list';
-import { parseDiff, Diff, Hunk } from 'react-diff-view';
-import { diffLines, formatLines } from 'unidiff';
+import { DiffListItem } from './DiffListItem';
 
 class DiffList extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      forceExpandedAllItems: false,
+      forcedOpenState: false,
     };
   }
 
-  /**
-   * Gets the difference between the two strings in a format for the
-   * react-diff-view lib
-   *
-   * @param {string} originalText
-   * @param {string} changedText
-   */
-  getGitDifference = (originalText, changedText) => {
-    const diffText = formatLines(diffLines(originalText, changedText), {
-      context: 3,
-    });
-    const [diff] = parseDiff(diffText, { nearbySequences: 'zip' });
-    return diff;
-  };
-
   renderItem = (index, key) => {
     const { items } = this.props;
+    const { forcedOpenState } = this.state;
+
     const diff = items[index];
 
-    const parsedDiff = this.getGitDifference(
-      diff.aFileContents,
-      diff.bFileContents
-    );
-
     return (
-      <Diff
-        key={key}
-        viewType="split"
-        diffType={parsedDiff.type}
-        hunks={parsedDiff.hunks}
-      >
-        {(hunks) =>
-          hunks.map((hunk) => <Hunk key={hunk.content} hunk={hunk} />)
-        }
-      </Diff>
+      <div key={key}>
+        <DiffListItem forcedOpenState={forcedOpenState} diff={diff} />
+      </div>
     );
   };
 
--- src/components/difflist/DiffListItem.js
+++ src/components/difflist/DiffListItem.js
@@ -1,5 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { parseDiff, Diff, Hunk } from 'react-diff-view';
+import { diffLines, formatLines } from 'unidiff';
+import { Collapse, Button, Icon } from '@blueprintjs/core';
 
-export const DiffListItem = ({ isOpen }) => {
-  return <div>Item</div>;
+/**
+ * Gets the difference between the two strings in a format for the
+ * react-diff-view lib
+ *
+ * @param {string} originalText
+ * @param {string} changedText
+ */
+const getGitDifference = (originalText, changedText) => {
+  const diffText = formatLines(diffLines(originalText, changedText), {
+    context: 3,
+  });
+  const [diff] = parseDiff(diffText, { nearbySequences: 'zip' });
+  return diff;
+};
+
+export const DiffListItem = ({ forcedOpenState, diff }) => {
+  const [isOpen, setOpen] = useState(true);
+  const parsedDiff = getGitDifference(diff.aFileContents, diff.bFileContents);
+  const icon = isOpen ? 'chevron-up' : 'chevron-down';
+
+  return (
+    <div>
+      <Button
+        onClick={() => setOpen(!isOpen)}
+        minimal={true}
+        style={{ width: '100%', display: 'flex', justifyContent: 'flex-start' }}
+      >
+        <Icon icon={icon} />
+      </Button>
+      <Collapse isOpen={isOpen}>
+        <Diff
+          viewType="split"
+          diffType={parsedDiff.type}
+          hunks={parsedDiff.hunks}
+        >
+          {(hunks) =>
+            hunks.map((hunk) => <Hunk key={hunk.content} hunk={hunk} />)
+          }
+        </Diff>
+      </Collapse>
+    </div>
+  );
 };
