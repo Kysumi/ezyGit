baff38d8e62d766e1ea8882d7bf9d73ddb5f14a6
Maunder <maunderscott@gmail.com>
2020-08-23 14:32:32 +1200

    Updating stage button style

--- src/components/diffList/buttons/StageButton.js
+++ src/components/diffList/buttons/StageButton.js
@@ -2,5 +2,12 @@ import React from 'react';
 import { Button } from '@blueprintjs/core';
 
 export const StageButton = ({ onClick }) => {
-  return <Button text="Stage" intent="primary" style={{ margin: '5px' }} />;
+  return (
+    <Button
+      text="Stage"
+      intent="primary"
+      style={{ margin: '5px' }}
+      onClick={onClick}
+    />
+  );
 };
