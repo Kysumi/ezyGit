ed1f3e68cd4e50de183ac5856b3cbfc331a2f059
Maunder <maunderscott@gmail.com>
2020-08-30 17:41:18 +1200

    Adding collapse header component and adding styling to more align with the original design

---
+++ src/components/diffList/CollapseHeader.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import { Collapse, Icon, Text } from '@blueprintjs/core';
+
+const containerStyle = {
+  boxShadow: '2px 2px 9px 0px rgba(50, 50, 50, 0.4)',
+  borderRadius: '5px',
+};
+
+const buttonStyle = {
+  width: '100%',
+  display: 'flex',
+  justifyContent: 'flex-start',
+  marginRight: '10px',
+  marginLeft: '10px',
+  marginTop: '10px',
+  alignContent: 'center',
+};
+
+const spacerStyle = {
+  width: '2px',
+  backgroundColor: '#767A76',
+  marginRight: '10px',
+  marginLeft: '10px',
+};
+
+const horizontalStyle = {
+  width: '100%',
+  height: '2px',
+  backgroundColor: '#767A76',
+};
+
+const titleStyle = {
+  display: 'flex',
+  alignItems: 'center',
+};
+
+export const CollapseHeader = ({ title, children }) => {
+  const [isOpen, setOpen] = useState(true);
+  const icon = isOpen ? 'chevron-down' : 'chevron-right';
+
+  return (
+    <div style={containerStyle}>
+      <div onClick={() => setOpen(!isOpen)} style={buttonStyle}>
+        <div style={{ padding: '10px' }}>
+          <Icon icon={icon} iconSize={24} color={'#647C90'} />
+        </div>
+        <div style={spacerStyle} />
+        <span style={titleStyle}>
+          <b>{title}</b>
+        </span>
+      </div>
+      {isOpen ? <div style={horizontalStyle} /> : null}
+      <Collapse isOpen={isOpen} keepChildrenMounted={true}>
+        {children}
+      </Collapse>
+    </div>
+  );
+};
--- src/components/diffList/DiffCollection.js
+++ src/components/diffList/DiffCollection.js
@@ -1,30 +1,13 @@
-import React, { useState } from 'react';
-import { Collapse, Button, Icon, Text } from '@blueprintjs/core';
+import React from 'react';
 import DiffList from './DiffList';
-const buttonStyle = {
-  width: '100%',
-  display: 'flex',
-  justifyContent: 'flex-start',
-};
+import { CollapseHeader } from './CollapseHeader';
 
 export const DiffCollection = ({ title, diffs, diffType }) => {
-  const [isOpen, setOpen] = useState(true);
-
-  const icon = isOpen ? 'chevron-up' : 'chevron-down';
-
   return (
-    <div>
-      <Button onClick={() => setOpen(!isOpen)} style={buttonStyle}>
-        <div style={buttonStyle}>
-          <Icon icon={icon} />
-          <Text>{title}</Text>
-        </div>
-      </Button>
-      <Collapse isOpen={isOpen} keepChildrenMounted={true}>
-        <div style={{ paddingLeft: '10px', backgroundColor: '#e6eaed' }}>
-          <DiffList items={diffs} diffType={diffType} />
-        </div>
-      </Collapse>
-    </div>
+    <CollapseHeader title={title}>
+      <div style={{ paddingLeft: '10px', backgroundColor: '#e6eaed' }}>
+        <DiffList items={diffs} diffType={diffType} />
+      </div>
+    </CollapseHeader>
   );
 };
