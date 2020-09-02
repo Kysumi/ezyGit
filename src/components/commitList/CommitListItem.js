47dcdabf10036c83a48123d60441bfd93646b2f9
Maunder <maunderscott@gmail.com>
2020-09-02 18:01:32 +1200

    Using styled themes for the commitlistitem

--- src/components/commitList/CommitListItem.js
+++ src/components/commitList/CommitListItem.js
@@ -3,25 +3,36 @@ import { Classes, Text } from '@blueprintjs/core';
 import { useSelector } from 'react-redux';
 import { stringToColour } from '../../helper/stringToColor';
 import { getBranchNameSelector } from '../../store/repo/RepoSelector';
+import styled, { ThemeProvider } from 'styled-components';
+
 const classNames = require('classnames');
+
+const Thingy = styled.div`
+  margin: 5px;
+  box-shadow: 10px 20px 32px -5px rgba(194, 190, 194, 1);
+  border-left: 10px solid ${(props) => props.theme.color};
+  border-radius: 5px;
+  background-color: ${(props) =>
+    props.theme.selected ? '#e6eaed' : '#ffffff'};
+  padding: 20px;
+`;
 
 export const CommitListItem = ({ isSelected, commit, onClick }) => {
   const color = stringToColour(useSelector(getBranchNameSelector));
 
-  const style = {
-    margin: '5px',
-    boxShadow: '10px 20px 32px -5px rgba(194,190,194,1)',
-    borderLeft: '10px solid ' + color,
-    borderRadius: '5px',
-    backgroundColor: isSelected ? '#e6eaed' : '#ffffff',
+  const themeSettings = {
+    color: color,
+    selected: isSelected,
   };
 
   return (
-    <div className={Classes.CARD} style={style} onClick={onClick}>
-      <Text className={classNames(Classes.TEXT_MUTED, Classes.TEXT_SMALL)}>
-        {commit.committer.name}
-      </Text>
-      <Text ellipsize={true}>{commit.message}</Text>
-    </div>
+    <ThemeProvider theme={themeSettings}>
+      <Thingy onClick={onClick} color={color}>
+        <Text className={classNames(Classes.TEXT_MUTED, Classes.TEXT_SMALL)}>
+          {commit.committer.name}
+        </Text>
+        <Text ellipsize={true}>{commit.message}</Text>
+      </Thingy>
+    </ThemeProvider>
   );
 };
