4f4f13a277813e341ec2d9b7df269b16c8bf6496
Kysumi <maunderscott@gmail.com>
2020-09-18 13:44:42 +1200

    Testing out solution from gitHub

--- .webpack.config.js
+++ .webpack.config.js
@@ -1,4 +1,31 @@
-module.exports = (config) => {
-  config.target = 'electron-renderer';
-  return config;
-};
+const path = require('path');
+const CircularDependencyPlugin = require('circular-dependency-plugin');
+const webpack = require('webpack');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = [
+  {
+    target: 'web',
+    entry: './src/js/index.js',
+    output: {
+      filename: 'script.js',
+      path: path.resolve(__dirname, 'app/js'),
+    },
+    plugins: [
+      new CircularDependencyPlugin(),
+      new webpack.ExternalsPlugin('commonjs', ['electron']),
+    ],
+  },
+  {
+    target: 'electron-main',
+    entry: './electron/main.js',
+    output: {
+      filename: 'index.js',
+      path: path.resolve(__dirname, 'app'),
+    },
+    plugins: [new CircularDependencyPlugin()],
+    externals: [nodeExternals()],
+  },
+];
+
+// https://github.com/electron/electron/issues/21457
