a68371dbbf4d37c1bdca5e99ec3baa988ad8ce4f
Maunder <maunderscott@gmail.com>
2020-09-20 12:10:28 +1200

    Removing electron and giving nw.js a go

--- electron/registerIpcHooks.js
+++ electron/registerIpcHooks.js
@@ -1,58 +1,58 @@
-const ipc = require('electron').ipcMain;
+// const ipc = require('electron').ipcMain;
 
-ipc.on('gitPull', async (event, args) => {
-  const git = require('isomorphic-git');
-  const fs = require('fs');
-  const http = require('isomorphic-git/http/node');
+// ipc.on('gitPull', async (event, args) => {
+//   const git = require('isomorphic-git');
+//   const fs = require('fs');
+//   const http = require('isomorphic-git/http/node');
 
-  console.log('pullling!!!');
-  try {
-    await git.pull({
-      fs,
-      http: http,
-      dir: args.gitDir,
-      singleBranch: true,
-      fastForwardOnly: true,
-      author: {
-        email: 'asdas',
-        name: 'asdasd',
-      },
-    });
-  } catch (error) {
-    console.log('FAILED TO PULL CHANGES!!!!!!!!!!!!');
-    event.sender.send('gitPullCompleted', {
-      success: false,
-      message: error.message,
-    });
-  }
-  console.log('done pulling');
+//   console.log('pullling!!!');
+//   try {
+//     await git.pull({
+//       fs,
+//       http: http,
+//       dir: args.gitDir,
+//       singleBranch: true,
+//       fastForwardOnly: true,
+//       author: {
+//         email: 'asdas',
+//         name: 'asdasd',
+//       },
+//     });
+//   } catch (error) {
+//     console.log('FAILED TO PULL CHANGES!!!!!!!!!!!!');
+//     event.sender.send('gitPullCompleted', {
+//       success: false,
+//       message: error.message,
+//     });
+//   }
+//   console.log('done pulling');
 
-  event.sender.send('gitPullCompleted', {
-    success: true,
-    message: ':)',
-  });
-});
+//   event.sender.send('gitPullCompleted', {
+//     success: true,
+//     message: ':)',
+//   });
+// });
 
-ipc.on('gitPush', async (event, args) => {
-  const git = require('isomorphic-git');
-  const fs = require('fs');
-  const http = require('isomorphic-git/http/node');
+// ipc.on('gitPush', async (event, args) => {
+//   const git = require('isomorphic-git');
+//   const fs = require('fs');
+//   const http = require('isomorphic-git/http/node');
 
-  try {
-    await git.push({
-      fs,
-      http: http,
-      dir: args.gitDir,
-    });
-  } catch (error) {
-    event.sender.send('gitPushCompleted', {
-      success: false,
-      message: error.message,
-    });
-  }
+//   try {
+//     await git.push({
+//       fs,
+//       http: http,
+//       dir: args.gitDir,
+//     });
+//   } catch (error) {
+//     event.sender.send('gitPushCompleted', {
+//       success: false,
+//       message: error.message,
+//     });
+//   }
 
-  event.sender.send('gitPushCompleted', {
-    success: true,
-    message: ':)',
-  });
-});
+//   event.sender.send('gitPushCompleted', {
+//     success: true,
+//     message: ':)',
+//   });
+// });
