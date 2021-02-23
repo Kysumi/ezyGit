dba95efcc3166dd020927392db9b6f047cd7cbc0
Maunder <maunderscott@gmail.com>
2021-02-23 22:25:03 +1300

    Starting to make the appbar

--- src/renderer/ui/components/AppBar/AppBar.tsx
+++ src/renderer/ui/components/AppBar/AppBar.tsx
@@ -1,12 +1,53 @@
 import React from 'react';
 import styled from 'styled-components';
+import { stringToColour } from '../../../helpers/stringToColor';
 
-const Title = styled.h1``;
+const Container = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: row;
+  padding: 30px;
+`;
+
+const Title = styled.h1`
+  margin: 0px;
+  padding-right: 30px;
+`;
+
+const Divider = styled.div`
+  width: 5px;
+  background-color: gray;
+  margin-right: 30px;
+`;
+
+const Branch = styled.div`
+  font-weight: 500;
+  padding: 10px;
+`;
+
+const Pill = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  border-radius: 5px;
+  background-color: ${stringToColour('default')};
+`;
+
+const BranchPill = () => {
+  return (
+    <Pill>
+      <Branch>default</Branch>
+    </Pill>
+  );
+};
 
 export const AppBar = () => {
   return (
-    <div>
+    <Container>
       <Title>ezyGit</Title>
-    </div>
+      <Divider />
+      <BranchPill />
+    </Container>
   );
 };
